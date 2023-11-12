70b9a66563b3021cf99ccd505f53ee0aadbc9aed
MonicaRinconGN <101375493+MonicaRinconGN@users.noreply.github.com>
2023-11-11 19:10:34 -0600

    carpeta WebApi

---
+++ AplicacionLibreria/WebApi/Controllers/AutorController.cs
@@ -0,0 +1,38 @@
+﻿using AccesoDatos.Operaciones;
+using AccesoDatos.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+/*
+ * la clase AutorController maneja las solicitudes HTTP relacionadas 
+ * con la entidad author
+ */
+namespace WebApi.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class AutorController : ControllerBase
+    {
+        // creamos una instancia de AutorDAO para manejar las operaciones relacionadas con la entidad author en la base de datos
+        private AutorDAO autorDAO = new AutorDAO();
+
+        /**
+         * EndPoint para insertar un nuevo autor
+         */
+        [HttpPost("autor")]
+        public bool insertarAutor([FromBody] Author autor)
+        {
+            return autorDAO.agregar(autor.Name);
+        }
+
+
+        /**
+         * EndPoint para obtener la lista de todos los autores
+         */
+        [HttpGet("autores")]
+        public List<Author> obtenerAutores()
+        {
+            return autorDAO.obternerTodos();
+        }
+    }
+}
---
+++ AplicacionLibreria/WebApi/Controllers/LibroController.cs
@@ -0,0 +1,59 @@
+﻿using AccesoDatos.Operaciones;
+using AccesoDatos.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+/*
+ * la clase LibroController maneja las solicitudes HTTP relacionadas 
+ * con la entidad book
+ */
+
+namespace WebApi.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class LibroController : ControllerBase
+    {
+        // creamos una instancia de LibroDAO para manejar las operaciones relacionadas con la entidad book en la base de datos
+        private LibroDAO libroDAO = new LibroDAO();
+
+        /**
+         * EndPoint para insertar un nuevo libro
+         */
+        [HttpPost("libro")]
+        public bool insertarLibro([FromBody] Book libro)
+        {
+            return libroDAO.agregar(libro.IdAuthor, libro.Title, libro.Chapters, libro.Pages, libro.Price);
+        }
+
+
+        /**
+         * EndPoint para obtener la lista de todos los libros
+         */
+        [HttpGet("libros")]
+        public List<Book> obtenerLibros()
+        {
+            return libroDAO.obtenerTodos();
+        }
+
+
+        /**
+         * EndPoint para obtener un libro específico por su id
+         */
+        [HttpGet("libro")]
+        public Book obtenerLibro(int id)
+        {
+            return libroDAO.consultar(id);
+        }
+
+
+        /**
+         * EndPoint para obtener los datos de los libros (incluyendo el autor)
+         */
+        [HttpGet("libroAutor")]
+        public List<BookAuthor> obtenerLibroAutor()
+        {
+            return libroDAO.obtenerLibroA();
+        }
+    }
+}
